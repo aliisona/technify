b282117e8c615188a9a355937af58d59303fd9a5
alisona <alile24@bergen.org>
2022-08-02 13:14:47 -0400

    cocoa touch file for home page created + outlet created for progress bar #TODO-- code the increasing progress when pressed or interacting with a post

---
+++ technify/HomePageViewController.swift
@@ -0,0 +1,31 @@
+//
+//  HomePageViewController.swift
+//  technify
+//
+//  Created by Scholar on 8/2/22.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    @IBOutlet weak var taskProgressBar: UIProgressView! //refer to when completing taskss
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
