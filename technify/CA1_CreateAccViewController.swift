963224d77a7d4cc4127102e48c547d1a93b3eb0c
alisona <alile24@bergen.org>
2022-08-14 01:46:18 -0400

    made creating a profile a lot harder (for fun)

--- technify/CA1_CreateAccViewController.swift
+++ technify/CA1_CreateAccViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+var completedChecks = 4
+
 public var user1 = Profile(usernameSet: "", emailSet: "", passwordSet: "", focusSet: "")
 
 class CA1_CreateAccViewController: UIViewController {
@@ -22,6 +24,8 @@ class CA1_CreateAccViewController: UIViewController {
     @IBOutlet weak var tacSwitch: UISwitch!
     
     @IBOutlet weak var subSwitch: UISwitch!
+    
+    @IBOutlet weak var mustCompleteLabel: UILabel!
     
     var passBtnTap : Bool =  false
     
@@ -47,21 +51,120 @@ class CA1_CreateAccViewController: UIViewController {
     }
     
     @IBAction func createAccountTapped(_ sender: UIButton) {
-        
-        if tacSwitch.isOn {
         user1 = Profile(usernameSet: usernameTextField.text! , emailSet: emailTextField.text!, passwordSet: passwordTextField.text!, focusSet: "NonSelected")
-        }
 
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        mustCompleteLabel.isHidden = true
+        //defaulted to be off
+        
         passwordTextField.isSecureTextEntry = true
-
+        usernameTextField.autocorrectionType = .no
+        passwordTextField.autocorrectionType = .no
+        emailTextField.autocorrectionType = .no
+        
         // Do any additional setup after loading the view.
     }
     
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if identifier == "CA1_CA2"{
+        
+        //checks
+            let usernameSuccess = usernameChecker()
+            
+            let passwordSuccess = passwordChecker()
+            
+//            let emailText = emailTextField.text!
+            
+            if !usernameSuccess {
+                usernameChecker()
+            }
+            
+            else if emailTextField.text == "" {
+                changeMustCompleteLabel(errorText: "Enter an email.")
+            }
+            else if !passwordSuccess {
+                passwordChecker()
+            }
+        //t&c check
+            else if !tacSwitch.isOn{
+           //alert "input value"
+                changeMustCompleteLabel(errorText: "Must accept terms and conditions.")            }
+            else  {
+                return true
+            }
+            
+        }
+        
+        return false
+        
+    }
+        
+    func changeMustCompleteLabel(errorText : String) {
+        mustCompleteLabel.isHidden = false
+        mustCompleteLabel.text = errorText
+    }
     
+    func usernameChecker() -> Bool {
+        let userText = usernameTextField.text!
+        var isSuccessful = false
+        
+        if userText == "" {
+            changeMustCompleteLabel(errorText: "Enter a username.")
+        }
+        
+        else if userText.count < 3 {
+            changeMustCompleteLabel(errorText: "Usernames must be longer than 3 characters.")
+        }
+        
+        else if userText.contains(",") {
+            changeMustCompleteLabel(errorText: "Usernames cannot contain ','.")
+        }
+        
+        else {
+            isSuccessful = true
+        }
+        
+        return isSuccessful
+    }
+    
+    func passwordChecker() -> Bool {
+        let passwordText = passwordTextField.text!
+        var isSuccessful = false
+        
+        var neededChar = false
+        let neededCharList = ["!", "@", "&"]
+        
+        if passwordText == "" {
+            changeMustCompleteLabel(errorText: "Create a password.")
+        }
+        
+        else if passwordText.count < 6 {
+            changeMustCompleteLabel(errorText: "Passwords must be at least 6 characters.")
+        }
+        
+//        else if !neededChar {
+//            for i in 0...passwordText.count-1 {
+//                if passwordText.contains(neededCharList[i]) {
+//                 neededChar = true
+//                }
+//                else {
+//                    changeMustCompleteLabel(errorText: "Usernames must contain at least one special character.")
+//                }
+//            }
+//          }
+        
+        else {
+            isSuccessful = true
+        }
+        
+        return isSuccessful
+    }
+            
+}//closeclass
+
     
 
     /*
@@ -74,4 +177,3 @@ class CA1_CreateAccViewController: UIViewController {
     }
     */
 
-}
